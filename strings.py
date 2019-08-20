7b500a99c0b5606c079588ebb8cbb63c1314b2c4
TylerCEdge <tylercedge@gmail.com>
2019-08-19 23:55:01 -0400

    assigning variables and concatenating also noticed emmet isnt working for python at the moment

--- strings.py
+++ strings.py
@@ -1,5 +1,11 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Tyler'
+age = 29
+
+# Concatenate - to insert a variable into a string
+
+print('Hello, my name is ' + name + ' and I am ' + str(age))
 
 # String Formatting
 
